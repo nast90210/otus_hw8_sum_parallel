a306e25b2ec7cd94eec75295248d186cc7c5a8e1
Mikhail <mikhail88@protonmail.com>
2021-12-05 00:58:52 +0300

    Initiate commit

---
+++ otus_hw9_sum_parallel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace otus_hw9_sum_parallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
